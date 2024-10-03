fe5648915d6ada3d19dee441a3eebc8dc6041f76
AssaBaradji <baradjiassa695@gmail.com>
2024-10-03 11:01:24 +0200

    update translation

--- src/views/recette/EditRecette.vue
+++ src/views/recette/EditRecette.vue
@@ -32,25 +32,25 @@ const saveRecipe = async () => {
     <div class="p-4 bg-light rounded shadow-sm">
       <div class="input-group mb-4">
         <span class="input-group-text bg-warning text-dark fw-bold">
-          <i class="fas fa-pen"></i>&nbsp;Titre de la recette
+          <i class="fas fa-pen"></i>&nbsp;{{ t('recipes.edit_form.tilte') }}
         </span>
         <input type="text" class="form-control" placeholder="Titre de la recette" v-model="newRecipe.title" />
       </div>
 
       <div class="input-group mb-4">
         <span class="input-group-text bg-warning text-dark fw-bold">
-          <i class="fas fa-utensils"></i>&nbsp;Type
+          <i class="fas fa-utensils"></i>&nbsp;{{ t('recipes.edit_form.type') }}
         </span>
         <select class="form-select" v-model="newRecipe.type">
-          <option value="Entrée">Entrée</option>
-          <option value="Plat">Plat Principal</option>
-          <option value="Dessert">Dessert</option>
+          <option value="Entrée">{{ t('recipes.edit_form.option_Entrée') }}</option>
+          <option value="Plat">{{ t('recipes.edit_form.option_plat') }}</option>
+          <option value="Dessert">{{ t('recipes.edit_form.option_dessert') }}</option>
         </select>
       </div>
 
       <div class="input-group mb-4">
         <span class="input-group-text bg-warning text-dark fw-bold">
-          <i class="fas fa-carrot"></i>&nbsp;Ingrédients
+          <i class="fas fa-carrot"></i>&nbsp;{{ t('recipes.edit_form.ingredients') }}
         </span>
         <input type="text" class="form-control" placeholder="Liste des ingrédients (séparés par des virgules)"
           v-model="newRecipe.ingredients" />
@@ -58,7 +58,7 @@ const saveRecipe = async () => {
 
       <div class="input-group mb-4">
         <span class="input-group-text bg-warning text-dark fw-bold">
-          <i class="fas fa-tags"></i>&nbsp;Catégorie
+          <i class="fas fa-tags"></i>&nbsp;{{ t('recipes.edit_form.categorie') }}
         </span>
         <select class="form-select" v-model="newRecipe.categoryId">
           <option v-for="category in categoryStore.categories" :key="category.id" :value="category.id">
@@ -68,7 +68,7 @@ const saveRecipe = async () => {
       </div>
 
       <button class="btn btn-warning w-100 fw-bold" @click="saveRecipe">
-        <i class="fas fa-save"></i> Sauvegarder
+        <i class="fas fa-save"></i> {{ t('recipes.edit_form.button') }}
       </button>
     </div>
   </div>
